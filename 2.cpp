6295c4f395da4ca809ef57359c723f14903e3b4b
DanilaPonomarev <96246197+DanilaPonomarev@users.noreply.github.com>
2022-01-09 16:01:32 +0300

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "iostream"
+#include <conio.h>
+#include "locale.h"
+#include "stdio.h"
+#include "string.h"
+ using namespace std;{
+double pas1[] = {55.0,123.0,131};
+    for(int i=0;i!=sizeof(pas1l)/sizeof(double);i++)
+        printf("arr[%d]=%lf\n",i,pas1[i]);
+    _getch();
+    system("cls");
+    return 0;
