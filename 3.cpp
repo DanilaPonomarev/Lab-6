b780e4e8c7bff355ca611b1de2615921ddf6c959
DanilaPonomarev <96246197+DanilaPonomarev@users.noreply.github.com>
2022-01-09 16:03:55 +0300

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int arr[5] = {5, 4, 3, 2, 1};
+    for(int i = 0; i < 5; ++i)
+    cout << "arr= " << arr[i] << endl; //тип int.
+    for(int i = 0; i < 5; ++i)
+    cout << "&arr[" << i << "]=" << &arr[i] << endl; //интервал адресов = 4, что соответсвует размеру резервируемое памятью под переменную типа int (4 байта).
+    for(int i = 0; i < 5; ++i)
+    *(arr + i) = i + 1;
+    for(int i = 0; i < 5; ++i)
+    cout << "arr= " << arr[i] << endl;
+    //(arr+0) = (arr + 2);  //Происходит ошибка.
+    int* pArr;
+    pArr = (arr + 2);
+    for(int i = 0; i < 2; ++i)
+        cout << "*pArr= " << *(pArr + i) << endl; //дальше смысла будет мало идти т.к. вам могут попасться любые типы данных и даже те на которые у вас не будет прав доступа.
+    getch();
+}
